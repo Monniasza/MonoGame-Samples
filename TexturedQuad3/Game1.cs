6dbfe153e1b3a9bd349c3093cf478d9b72c20e77
Monniasza <37037712+Monniasza@users.noreply.github.com>
2025-06-08 15:20:09 +0200

    remade the textured quad example

---
+++ TexturedQuad3/Game1.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TexturedQuad
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        Quad quad;
+        VertexDeclaration vertexDeclaration;
+        Matrix View, Projection;
+        protected override void Initialize()
+        {
+            quad = new Quad(Vector3.Zero, Vector3.Backward, Vector3.Up, 1, 1);
+            View = Matrix.CreateLookAt(new Vector3(0, 0, 2), Vector3.Zero,
+                Vector3.Up);
+            Projection = Matrix.CreatePerspectiveFieldOfView(
+                MathHelper.PiOver4, 4.0f / 3.0f, 1, 500);
+
+            base.Initialize();
+        }
+
+        Texture2D texture;
+        BasicEffect quadEffect;
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            texture = Content.Load<Texture2D>("Glass");
+            quadEffect = new BasicEffect(graphics.GraphicsDevice);
+            // We still do not have lighting implemented in the shaders
+            //quadEffect.EnableDefaultLighting();
+
+            quadEffect.World = Matrix.Identity;
+            quadEffect.View = View;
+            quadEffect.Projection = Projection;
+            quadEffect.TextureEnabled = true;
+            quadEffect.Texture = texture;
+
+            vertexDeclaration = new VertexDeclaration(new VertexElement[]
+                {
+                    new VertexElement(0, VertexElementFormat.Vector3, VertexElementUsage.Position, 0),
+                    new VertexElement(12, VertexElementFormat.Vector3, VertexElementUsage.Normal, 0),
+                    new VertexElement(24, VertexElementFormat.Vector2, VertexElementUsage.TextureCoordinate, 0)
+                }
+            );
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            foreach (EffectPass pass in quadEffect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+
+                GraphicsDevice.DrawUserIndexedPrimitives
+                    <VertexPositionNormalTexture>(
+                    PrimitiveType.TriangleList,
+                    quad.Vertices, 0, 4,
+                    quad.Indexes, 0, 2);
+            }
+
+            base.Draw(gameTime);
+        }
+    }
+}
